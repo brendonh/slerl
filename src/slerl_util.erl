131cc12c168380b66bec5d373152206c786091c9
Brendon Hogger <brendonh@gmail.com>
2010-02-07 05:15:57 +0800

    Sim connection notes, zero encoding functions

--- src/slerl_util.erl
+++ src/slerl_util.erl
@@ -7,7 +7,11 @@
 %%%-------------------------------------------------------------------
 -module(slerl_util).
 
--compile([export_all]).
+-export([macaddr/0, macaddr/1, 
+         md5_hex/1, 
+         parse_message_template/0,
+         zero_encode/1, zero_decode/1
+         ]).
 
 
 -define(DBG(T), io:format("~p ~p~n", [self(), T])).
@@ -42,3 +46,39 @@ parse_message_template() ->
     Content = slerl_message_template_lexer:scan_file("priv/message_template.msg"),
     {ok, {_Version, Messages}} = slerl_message_template_parser:parse(Content),
     ?DBG(Messages).
+
+
+
+zero_encode(B) ->
+    zero_encode(B, []).
+
+zero_encode(<<>>, Buff) ->
+    list_to_binary(lists:reverse(Buff));
+zero_encode(<<0:8/integer, Rest/binary>>, Buff) ->
+    {Run, Rest2} = run_zeros(Rest, 1),
+    zero_encode(Rest2, [Run|Buff]);
+zero_encode(<<I:8/integer, Rest/binary>>, Buff) ->
+    zero_encode(Rest, [I|Buff]).
+
+
+run_zeros(Rest, Count) when Count == 255 ->
+    {<<0:8/integer, Count:8/integer>>, Rest};
+run_zeros(<<0:8/integer, Rest/binary>>, Count) -> 
+    run_zeros(Rest, Count+1);
+run_zeros(Rest, Count) -> 
+    {<<0:8/integer, Count:8/integer>>, Rest}.
+
+
+zero_decode(B) ->
+    zero_decode(B, []).
+
+zero_decode(<<>>, Buff) ->
+    list_to_binary(lists:reverse(Buff));
+zero_decode(<<0:8/integer, Count:8/integer, Rest/binary>>, Buff) ->
+    Bits = Count * 8,
+    zero_decode(Rest, [<<0:Bits/integer>>|Buff]);
+zero_decode(<<I:8/integer, Rest/binary>>, Buff) ->
+    zero_decode(Rest, [I|Buff]).
+
+
+    
